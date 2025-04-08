6ba41d0186c62dc9783334b201810081fc1e9876
Xuho <ngotranxuanhoa09062004@gmail.com>
2025-04-08 09:48:05 +0700

    modify path vosk

--- Fish_Manage/obj/Debug/net8.0/Fish_Manage.AssemblyInfo.cs
+++ Fish_Manage/obj/Debug/net8.0/Fish_Manage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fish_Manage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66fa24acca3fac888f419c60dbbd4a6e84024236")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c988eac12748677a2d6256c204a95c8737e429b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fish_Manage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fish_Manage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
